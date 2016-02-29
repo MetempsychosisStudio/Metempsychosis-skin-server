801197701c74f76892a9e2a6e3e060dc480257e2
simon3000 <simon3000@163.com>
2016-02-29 19:16:30 +0800

    登录基本框架

---
+++ script/login.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+function login(username, password) {
+    fs.readFile('./data/players.json', function(fileData) {
+        var userInfo = JSON.parse(fileData)
+    })
+}
+
+fs.exists('./data/players.json', function(exists) {
+    if (exists == false) {
+        fs.writeFile('./data/players.json', '[]', function(err) {
+            if (err) throw err;
+            console.log('用户登陆数据创建成功');
+            login()
+        });
+    };
+});
+
+module.exports = login;
