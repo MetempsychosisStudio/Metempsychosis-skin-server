fd63ee623a894c8f26559faed2cf7dafc1e56256
simon3000 <simon3000@163.com>
2016-03-15 11:55:43 +0800

    update →_→

--- index.js
+++ index.js
@@ -8,102 +8,9 @@ let app = express()
 let userScript
 var setting
 
-function openServer() {
-    setting = require('./config.js');
-    console.log("配置文件读取成功");
-    app.all(/json/, compression(), function(req, res, next) {
-        res.end(userScript.getJSON('simon3000'))
-    });
-
-    app.post(/upload/, bodyParser.urlencoded({
-        extended: true
-    }), bodyParser.json(), function(req, res, next) {
-        userScript.login(req.body.username, req.body.pass)
-    });
-
-    app.post(/login/, bodyParser.urlencoded({
-        extended: true
-    }), bodyParser.json(), function(req, res, next) {
-        userScript.login(req.body.username, req.body.pass)
-    });
-
-    app.post(/register/, bodyParser.urlencoded({
-        extended: true
-    }), bodyParser.json(), function(req, res, next) {
-        res.end(userScript.reg(req.body.username, req.body.pass, req.body.rpass))
-    });
-
-    app.get(/indexcss/, compression(), function(req, res, next) {
-        res.sendFile('index.css', {
-            root: 'public/'
-        });
-    });
-
-    app.get(/indexjs/, compression(), function(req, res, next) {
-        res.sendFile('index.js', {
-            root: 'public/'
-        });
-    });
-
-    app.get(/indexsetting/, compression(), function(req, res, next) {
-        res.end('$("title").html("' + setting().interface.title + '")');
-    });
-
-    app.get(/favicon/, compression(), function(req, res, next) {
-        res.sendFile('favicon.ico', {
-            root: 'public/'
-        });
-    });
-
-    app.use(compression(), function(req, res, next) {
-        res.sendFile('index.html', {
-            root: 'public/'
-        });
-    });
-    serverDone()
-}
-
-function checkConfig() {
-    fs.stat('./config.js', function(err, stats) {
-        if (stats == undefined) {
-            console.log('创建配置文件...');
-            let defaultConfig = []
-            defaultConfig.push('"use strict";')
-            defaultConfig.push('let setting = {};setting.server = {};setting.interface = {}')
-            defaultConfig.push('//配置文件 (别改上面的)')
-            defaultConfig.push('')
-            defaultConfig.push('//服务器端口号')
-            defaultConfig.push('setting.server.port = 8080')
-            defaultConfig.push('')
-            defaultConfig.push('//网页标题')
-            defaultConfig.push('setting.interface.title = "皮肤服务器"')
-            defaultConfig.push('')
-            defaultConfig.push('')
-            defaultConfig.push('')
-            defaultConfig.push('')
-            defaultConfig.push('//不要改下面的东西')
-            defaultConfig.push('function settingOut() {')
-            defaultConfig.push('return setting')
-            defaultConfig.push('}')
-            defaultConfig.push('module.exports = settingOut;')
-            fs.writeFileSync('./config.js', defaultConfig.join('\n'));
-            console.log('config.js 创建成功');
-        }
-        openServer();
-    })
-}
-
-function serverDone() {
-    let server = app.listen(setting().server.port, function() {
-        let host = server.address().address;
-        let port = server.address().port;
-        console.log('服务器开启 http://%s:%s', host, port);
-    });
-}
-
-fs.stat('./data', function(err, stats) {
-    fs.stat('./data/players', function(err, playerStats) {
-        fs.stat('./data/players.json', function(err, JSONStats) {
+fs.stat('./data', (err, stats) => {
+    fs.stat('./data/players', (err, playerStats) => {
+        fs.stat('./data/players.json', (err, JSONStats) => {
             if (stats == undefined) {
                 fs.mkdirSync('./data')
             }
@@ -115,7 +22,87 @@ fs.stat('./data', function(err, stats) {
                 console.log('用户数据创建成功');
             }
             userScript = require('./script/reg.js')
-            checkConfig()
+
+            fs.stat('./config.js', (err, stats) => {
+                if (stats == undefined) {
+                    console.log('创建配置文件...');
+                    let defaultConfig = []
+                    defaultConfig.push('"use strict";')
+                    defaultConfig.push('let setting = {};setting.server = {};setting.interface = {}')
+                    defaultConfig.push('//配置文件 (别改上面的)')
+                    defaultConfig.push('')
+                    defaultConfig.push('//服务器端口号')
+                    defaultConfig.push('setting.server.port = 8080')
+                    defaultConfig.push('')
+                    defaultConfig.push('//网页标题')
+                    defaultConfig.push('setting.interface.title = "皮肤服务器"')
+                    defaultConfig.push('')
+                    defaultConfig.push('')
+                    defaultConfig.push('')
+                    defaultConfig.push('')
+                    defaultConfig.push('//不要改下面的东西')
+                    defaultConfig.push('module.exports = setting;')
+                    fs.writeFileSync('./config.js', defaultConfig.join('\n'));
+                    console.log('config.js 创建成功');
+                }
+
+                setting = require('./config.js');
+                console.log("配置文件读取成功");
+                app.all(/json/, compression(), (req, res, next) => {
+                    res.end(userScript.getJSON('simon3000'))
+                });
+
+                app.post(/upload/, bodyParser.urlencoded({
+                    extended: true
+                }), bodyParser.json(), (req, res, next) => {
+                    userScript.login(req.body.username, req.body.pass)
+                });
+
+                app.post(/login/, bodyParser.urlencoded({
+                    extended: true
+                }), bodyParser.json(), (req, res, next) => {
+                    userScript.login(req.body.username, req.body.pass)
+                });
+
+                app.post(/register/, bodyParser.urlencoded({
+                    extended: true
+                }), bodyParser.json(), (req, res, next) => {
+                    res.end(userScript.reg(req.body.username, req.body.pass, req.body.rpass))
+                });
+
+                app.get(/indexcss/, compression(), (req, res, next) => {
+                    res.sendFile('index.css', {
+                        root: 'public/'
+                    });
+                });
+
+                app.get(/indexjs/, compression(), (req, res, next) => {
+                    res.sendFile('index.js', {
+                        root: 'public/'
+                    });
+                });
+
+                app.get(/indexsetting/, compression(), (req, res, next) => {
+                    res.end('$("title").html("' + setting.interface.title + '")');
+                });
+
+                app.get(/favicon/, compression(), (req, res, next) => {
+                    res.sendFile('favicon.ico', {
+                        root: 'public/'
+                    });
+                });
+
+                app.use(compression(), (req, res, next) => {
+                    res.sendFile('index.html', {
+                        root: 'public/'
+                    });
+                });
+                let server = app.listen(setting.server.port, () => {
+                    let host = server.address().address;
+                    let port = server.address().port;
+                    console.log('服务器开启 http://%s:%s', host, port);
+                });
+            })
         })
     })
 })
