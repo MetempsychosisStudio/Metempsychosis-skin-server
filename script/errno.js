ba7887163da96f3fd352b56800ae2a2912352adf
simon3000 <simon3000@163.com>
2016-03-23 19:09:18 +0800

    errno helper

---
+++ script/errno.js
@@ -0,0 +1,275 @@
+"use strict";
+
+let errno = (no) => {
+    switch (no) {
+        case 0:
+            return ('errno: ' + no + ' (Success)');
+        case -1:
+            return ('errno: ' + no + ' (Operation not permitted)');
+        case -2:
+            return ('errno: ' + no + ' (No such file or directory)');
+        case -3:
+            return ('errno: ' + no + ' (No such process)');
+        case -4:
+            return ('errno: ' + no + ' (Interrupted system call)');
+        case -5:
+            return ('errno: ' + no + ' (Input/output error)');
+        case -6:
+            return ('errno: ' + no + ' (No such device or address)');
+        case -7:
+            return ('errno: ' + no + ' (Argument list too long)');
+        case -8:
+            return ('errno: ' + no + ' (Exec format error)');
+        case -9:
+            return ('errno: ' + no + ' (Bad file descriptor)');
+        case -10:
+            return ('errno: ' + no + ' (No child processes)');
+        case -11:
+            return ('errno: ' + no + ' (Resource temporarily unavailable)');
+        case -12:
+            return ('errno: ' + no + ' (Cannot allocate memory)');
+        case -13:
+            return ('errno: ' + no + ' (Permission denied)');
+        case -14:
+            return ('errno: ' + no + ' (Bad address)');
+        case -15:
+            return ('errno: ' + no + ' (Block device required)');
+        case -16:
+            return ('errno: ' + no + ' (Device or resource busy)');
+        case -17:
+            return ('errno: ' + no + ' (File exists)');
+        case -18:
+            return ('errno: ' + no + ' (Invalid cross-device link)');
+        case -19:
+            return ('errno: ' + no + ' (No such device)');
+        case -20:
+            return ('errno: ' + no + ' (Not a directory)');
+        case -21:
+            return ('errno: ' + no + ' (Is a directory)');
+        case -22:
+            return ('errno: ' + no + ' (Invalid argument)');
+        case -23:
+            return ('errno: ' + no + ' (Too many open files in system)');
+        case -24:
+            return ('errno: ' + no + ' (Too many open files)');
+        case -25:
+            return ('errno: ' + no + ' (Inappropriate ioctl for device)');
+        case -26:
+            return ('errno: ' + no + ' (Text file busy)');
+        case -27:
+            return ('errno: ' + no + ' (File too large)');
+        case -28:
+            return ('errno: ' + no + ' (No space left on device)');
+        case -29:
+            return ('errno: ' + no + ' (Illegal seek)');
+        case -30:
+            return ('errno: ' + no + ' (Read-only file system)');
+        case -31:
+            return ('errno: ' + no + ' (Too many links)');
+        case -32:
+            return ('errno: ' + no + ' (Broken pipe)');
+        case -33:
+            return ('errno: ' + no + ' (Numerical argument out of domain)');
+        case -34:
+            return ('errno: ' + no + ' (Numerical result out of range)');
+        case -35:
+            return ('errno: ' + no + ' (Resource deadlock avoided)');
+        case -36:
+            return ('errno: ' + no + ' (File name too long)');
+        case -37:
+            return ('errno: ' + no + ' (No locks available)');
+        case -38:
+            return ('errno: ' + no + ' (Function not implemented)');
+        case -39:
+            return ('errno: ' + no + ' (Directory not empty)');
+        case -40:
+            return ('errno: ' + no + ' (Too many levels of symbolic links)');
+        case -41:
+            return ('errno: ' + no + ' (Unknown error 41)');
+        case -42:
+            return ('errno: ' + no + ' (No message of desired type)');
+        case -43:
+            return ('errno: ' + no + ' (Identifier removed)');
+        case -44:
+            return ('errno: ' + no + ' (Channel number out of range)');
+        case -45:
+            return ('errno: ' + no + ' (Level 2 not synchronized)');
+        case -46:
+            return ('errno: ' + no + ' (Level 3 halted)');
+        case -47:
+            return ('errno: ' + no + ' (Level 3 reset)');
+        case -48:
+            return ('errno: ' + no + ' (Link number out of range)');
+        case -49:
+            return ('errno: ' + no + ' (Protocol driver not attached)');
+        case -50:
+            return ('errno: ' + no + ' (No CSI structure available)');
+        case -51:
+            return ('errno: ' + no + ' (Level 2 halted)');
+        case -52:
+            return ('errno: ' + no + ' (Invalid exchange)');
+        case -53:
+            return ('errno: ' + no + ' (Invalid request descriptor)');
+        case -54:
+            return ('errno: ' + no + ' (Exchange full)');
+        case -55:
+            return ('errno: ' + no + ' (No anode)');
+        case -56:
+            return ('errno: ' + no + ' (Invalid request code)');
+        case -57:
+            return ('errno: ' + no + ' (Invalid slot)');
+        case -58:
+            return ('errno: ' + no + ' (Unknown error 58)');
+        case -59:
+            return ('errno: ' + no + ' (Bad font file format)');
+        case -60:
+            return ('errno: ' + no + ' (Device not a stream)');
+        case -61:
+            return ('errno: ' + no + ' (No data available)');
+        case -62:
+            return ('errno: ' + no + ' (Timer expired)');
+        case -63:
+            return ('errno: ' + no + ' (Out of streams resources)');
+        case -64:
+            return ('errno: ' + no + ' (Machine is not on the network)');
+        case -65:
+            return ('errno: ' + no + ' (Package not installed)');
+        case -66:
+            return ('errno: ' + no + ' (Object is remote)');
+        case -67:
+            return ('errno: ' + no + ' (Link has been severed)');
+        case -68:
+            return ('errno: ' + no + ' (Advertise error)');
+        case -69:
+            return ('errno: ' + no + ' (Srmount error)');
+        case -70:
+            return ('errno: ' + no + ' (Communication error on send)');
+        case -71:
+            return ('errno: ' + no + ' (Protocol error)');
+        case -72:
+            return ('errno: ' + no + ' (Multihop attempted)');
+        case -73:
+            return ('errno: ' + no + ' (RFS specific error)');
+        case -74:
+            return ('errno: ' + no + ' (Bad message)');
+        case -75:
+            return ('errno: ' + no + ' (Value too large for defined datatype)');
+        case -76:
+            return ('errno: ' + no + ' (Name not unique on network)');
+        case -77:
+            return ('errno: ' + no + ' (File descriptor in bad state)');
+        case -78:
+            return ('errno: ' + no + ' (Remote address changed)');
+        case -79:
+            return ('errno: ' + no + ' (Can not access a needed sharedlibrary)');
+        case -80:
+            return ('errno: ' + no + ' (Accessing a corrupted sharedlibrary)');
+        case -81:
+            return ('errno: ' + no + ' (.lib section in a.out corrupted)');
+        case -82:
+            return ('errno: ' + no + ' (Attempting to link in too manyshared libraries)');
+        case -83:
+            return ('errno: ' + no + ' (Cannot exec a shared librarydirectly)');
+        case -84:
+            return ('errno: ' + no + ' (Invalid or incomplete multibyte orwide character)');
+        case -85:
+            return ('errno: ' + no + ' (Interrupted system call should berestarted)');
+        case -86:
+            return ('errno: ' + no + ' (Streams pipe error)');
+        case -87:
+            return ('errno: ' + no + ' (Too many users)');
+        case -88:
+            return ('errno: ' + no + ' (Socket operation on non-socket)');
+        case -89:
+            return ('errno: ' + no + ' (Destinationaddress required)');
+        case -90:
+            return ('errno: ' + no + ' (Message too long)');
+        case -91:
+            return ('errno: ' + no + ' (Protocol wrong type for socket)');
+        case -92:
+            return ('errno: ' + no + ' (Protocol not available)');
+        case -93:
+            return ('errno: ' + no + ' (Protocol not supported)');
+        case -94:
+            return ('errno: ' + no + ' (Socket type not supported)');
+        case -95:
+            return ('errno: ' + no + ' (Operation not supported)');
+        case -96:
+            return ('errno: ' + no + ' (Protocol family not supported)');
+        case -97:
+            return ('errno: ' + no + ' (Address family not supported byprotocol)');
+        case -98:
+            return ('errno: ' + no + ' (Address already in use)');
+        case -99:
+            return ('errno: ' + no + ' (Cannot assign requested address)');
+        case -100:
+            return ('errno: ' + no + ' (Network is down)');
+        case -101:
+            return ('errno: ' + no + ' (Network is unreachable)');
+        case -102:
+            return ('errno: ' + no + ' (Network dropped connection onreset)');
+        case -103:
+            return ('errno: ' + no + ' (Software caused connection abort)');
+        case -104:
+            return ('errno: ' + no + ' (Connection reset by peer)');
+        case -105:
+            return ('errno: ' + no + ' (No buffer space available)');
+        case -106:
+            return ('errno: ' + no + ' (Transport endpoint is alreadyconnected)');
+        case -107:
+            return ('errno: ' + no + ' (Transport endpoint is notconnected)');
+        case -108:
+            return ('errno: ' + no + ' (Cannot send after transportendpoint shutdown)');
+        case -109:
+            return ('errno: ' + no + ' (Too many references: cannot splice)');
+        case -110:
+            return ('errno: ' + no + ' (Connection timed out)');
+        case -111:
+            return ('errno: ' + no + ' (Connection refused)');
+        case -112:
+            return ('errno: ' + no + ' (Host is down)');
+        case -113:
+            return ('errno: ' + no + ' (No route to host)');
+        case -114:
+            return ('errno: ' + no + ' (Operation already in progress)');
+        case -115:
+            return ('errno: ' + no + ' (Operation now in progress)');
+        case -116:
+            return ('errno: ' + no + ' (Stale NFS file handle)');
+        case -117:
+            return ('errno: ' + no + ' (Structure needs cleaning)');
+        case -118:
+            return ('errno: ' + no + ' (Not a XENIX named type file)');
+        case -119:
+            return ('errno: ' + no + ' (No XENIX semaphores available)');
+        case -120:
+            return ('errno: ' + no + ' (Is a named type file)');
+        case -121:
+            return ('errno: ' + no + ' (Remote I/O error)');
+        case -122:
+            return ('errno: ' + no + ' (Disk quota exceeded)');
+        case -123:
+            return ('errno: ' + no + ' (No medium found)');
+        case -124:
+            return ('errno: ' + no + ' (Wrong medium type)');
+        case -125:
+            return ('errno: ' + no + ' (Operation canceled)');
+        case -126:
+            return ('errno: ' + no + ' (Required key not available)');
+        case -127:
+            return ('errno: ' + no + ' (Key has expired)');
+        case -128:
+            return ('errno: ' + no + ' (Key has been revoked)');
+        case -129:
+            return ('errno: ' + no + ' (Key was rejected by service)');
+        case -130:
+            return ('errno: ' + no + ' (Owner died)');
+        case -131:
+            return ('errno: ' + no + ' (State not recoverable)');
+        case -132:
+            return ('errno: ' + no + ' (Operation not possible due toRF-kill)');
+        default:
+            return ('errno: ' + no + ' ( unknow error)');
+    }
+}
+module.exports = errno;
